138a3f190efaa1b033a34e639c1e1050cdd252b8
mFriesen <144160470+mfriesen1024@users.noreply.github.com>
2024-09-09 08:40:05 -0300

    Add input system and player stat manager

---
+++ Assets/Player/StatManager.cs
@@ -0,0 +1,15 @@
+﻿namespace Assets.Player
+{
+    /// <summary>
+    /// This manages the player's stats.
+    /// </summary>
+    internal class StatManager
+    {
+        public int hp, mp, xp, score;
+        public int lastCheckpoint;
+        /// <summary>
+        /// Progression indicator.
+        /// </summary>
+        public bool[] bricks;
+    }
+}
