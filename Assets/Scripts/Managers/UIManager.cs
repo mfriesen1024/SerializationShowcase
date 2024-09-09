cbdf039ddf3c9e55c88e06ce458790efc4c6dfc8
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-09-09 20:55:19 -0300

    Fix buttons

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -4,38 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 namespace Assets.Scripts.Managers
 {
     internal class UIManager:MonoBehaviour
     {
         [SerializeField]Canvas menu, hud;
-        [SerializeField] Button ng, lg, sc1, sc2, sc3;
+        [SerializeField]GameObject ngo,lgo,sc1o,sc2o,sc3o;
+        Button ng, lg, sc1, sc2, sc3;
         AreaManager am;
         public int UIState { get => uiState; set { SetUIState(value); } }
         int uiState;
 
         private void Start()
         {
-            am = GameManager.Instance.areaManager;
 
             UIState = 0;
 
+            ng = ngo.GetComponent<Button>();
+            lg = lgo.GetComponent<Button> ();
+            sc1 = sc1o.GetComponent<Button>();
+            sc2 = sc2o.GetComponent<Button>();
+            sc3 = sc3o.GetComponent<Button>();
+
             // if new game, load level 1 with no questions asked.
-            ng.clicked += () => { am.LoadScene(1); };
-            lg.clicked += () => { throw new NotImplementedException("aaa!"); };
-            sc1.clicked += () => { LoadButton(1); };
-            sc2.clicked += () => { LoadButton(2); };
-            sc3.clicked += () => { LoadButton(3); };
-        }        
+            ng.onClick.AddListener(() => { am.LoadScene(1); });
+            lg.onClick.AddListener(() => { throw new NotImplementedException("aaa!"); });
+            sc1.onClick.AddListener(() => { LoadButton(1); });
+            sc2.onClick.AddListener(() => { LoadButton(2); });
+            sc3.onClick.AddListener(() => { LoadButton(3); });
+
+            am = GameManager.Instance.areaManager;
+        }
 
         void SetUIState(int state)
         {
             switch(state)
             {
-                case 0: hud.gameObject.SetActive(false); Camera.main.enabled = false; menu.gameObject.SetActive(true); break;
-                case 1: hud.gameObject.SetActive(true); Camera.main.enabled = true; menu.gameObject.SetActive(false); break;
+                case 0: hud.gameObject.SetActive(false); if (Camera.main != null) { Camera.main.enabled = false; } menu.gameObject.SetActive(true); break;
+                case 1: hud.gameObject.SetActive(true); if (Camera.main != null) { Camera.main.enabled = false; } menu.gameObject.SetActive(false); break;
             }
 
             uiState = state;
