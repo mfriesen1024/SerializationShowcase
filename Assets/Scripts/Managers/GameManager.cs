f1c05b0b15b68a551eafbeb449cfa56e2a4d7bbf
mFriesen <144160470+mfriesen1024@users.noreply.github.com>
2024-09-09 11:52:02 -0300

    Create gameman

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using Assets.Scripts.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Diagnostics;
+
+namespace Assets.Scripts.Managers
+{
+    internal class GameManager:MonoBehaviour
+    {
+        /// <summary>
+        /// Represents the player.
+        /// </summary>
+        PlayerController pc;
+        /// <summary>
+        /// Used to save data.
+        /// </summary>
+        public DataManager dm;
+
+        public static GameManager Instance;
+
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            
+        }
+
+        private void FixedUpdate()
+        {
+            
+        }
+    }
+}
