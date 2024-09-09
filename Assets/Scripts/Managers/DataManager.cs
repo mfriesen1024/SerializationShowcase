9583954a6e3c9954379b928d6154c6ca07cfa6ec
mFriesen <144160470+mfriesen1024@users.noreply.github.com>
2024-09-09 09:59:03 -0300

    Create dataman and save

---
+++ Assets/Scripts/Managers/DataManager.cs
@@ -0,0 +1,77 @@
+﻿using Assets.Scripts.Player;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+namespace Assets.Scripts.Managers
+{
+    internal class DataManager
+    {
+        public PlayerController PlayerController;
+
+        string dataPath = Application.persistentDataPath + "\\saveData\\";
+        string playerFile = "player.dat";
+
+        /// <summary>
+        /// Should load and unpack all data from files.
+        /// </summary>
+        public void Load()
+        {
+            BinaryFormatter binaryFormatter = new();
+
+            LoadPlayer();
+
+            // Separate playerloading for my own sanity.
+            void LoadPlayer()
+            {
+                if (PlayerController == null)
+                {
+                    // if pc is null, make it known that this is problem.
+                    throw new InvalidOperationException("Cannot unpack playerdata to a nonexistent player.");
+                }
+
+                // Check if stuff exists, leave a bool as a flag for logging.
+                bool exists = false;
+                if (Directory.Exists(dataPath))
+                {
+                    if (File.Exists(dataPath + playerFile))
+                    {
+                        // if both dir and file exist, dont log the error.
+                        exists = true;
+
+                        // deserialize.
+                        FileStream fs = File.OpenRead(dataPath + playerFile);
+                        PlayerController.statMan = (PlayerSave)binaryFormatter.Deserialize(fs);
+                    }
+                }
+                else { Directory.CreateDirectory(dataPath); }
+
+                // It automatically creates a new save before this fails, just note that in logs.                    
+                if (!exists) { Debug.LogWarning("File didn't exist, assuming PC will auto create a new statman."); }
+            }
+        }
+
+        public void Save()
+        {
+            SavePlayer();
+
+            void SavePlayer()
+            {
+                BinaryFormatter binaryFormatter = new();
+
+                if (PlayerController == null)
+                {
+                    // if pc is null, make it known that this is problem.
+                    throw new InvalidOperationException("Cannot unpack playerdata to a nonexistent player.");
+                }
+
+                // This time, we're saving it as long as things aren't null, so dont quit if directory is nonexistent.
+                if (!Directory.Exists(dataPath)) { Directory.CreateDirectory(dataPath); }
+
+                FileStream fs = File.Create(dataPath + playerFile);
+                binaryFormatter.Serialize(fs, (PlayerSave)PlayerController.statMan);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerSave.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Player
+{
+    [Serializable]
+    internal class PlayerSave
+    {
+        public int hp = 10, mp = 10, xp = 0, score = 0;
+        public int lastCheckpoint = -1;
+        /// <summary>
+        /// Progression indicator.
+        /// </summary>
+        public bool[] bricks = new bool[3] { false, false, false };
+
+        public static implicit operator PlayerSave(StatManager statMan)
+        {
+            PlayerSave save = new();
+
+            save.hp = statMan.hp;
+            save.mp = statMan.mp;
+            save.xp = statMan.xp;
+            save.score = statMan.Score;
+
+            // progression stuff
+            save.lastCheckpoint = statMan.lastCheckpoint;
+            save.bricks = statMan.bricks;
+
+            // return stuff
+            return save;
+        }
+
+        // I think this needs to be implicit so I can cast to save, and it will implicitly cast to statman.
+        public static implicit operator StatManager(PlayerSave save)
+        {
+            StatManager statMan = new();
+
+            save.hp = statMan.hp;
+            save.mp = statMan.mp;
+            save.xp = statMan.xp;
+            save.score = statMan.Score;
+
+            // progression stuff
+            save.lastCheckpoint = statMan.lastCheckpoint;
+            save.bricks = statMan.bricks;
+
+            return statMan;
+        }
+    }
+}
