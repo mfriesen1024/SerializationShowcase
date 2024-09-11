b3d5cfc04f565c1529297eb177bc682d69b0c575
mFriesen <144160470+mfriesen1024@users.noreply.github.com>
2024-09-11 13:58:23 -0300

    Add tick counting

---
+++ Assets/Scripts/Managers/TickCounter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Assets.Scripts.Managers
+{
+    public class TickCounter
+    {
+        /// <summary>
+        /// How many updates should occur before execution.
+        /// </summary>
+        public int interval = 4;
+        /// <summary>
+        /// The action to execute when updating the tick.
+        /// </summary>
+        public Action action = () => { };
+
+        int tickCount = 0;
+
+        /// <summary>
+        /// Increment counter, check if we need to execute action.
+        /// </summary>
+        public void Tick()
+        {
+            if (tickCount == 0) { action(); }
+            tickCount++;
+            if (tickCount == interval) { tickCount = 0; }
+        }
+    }
+}
